6fcbaf67eb0baf7ae18f9ce46cae78aef709cc7f
Charles Northup <cjnorth40@gmail.com>
2014-03-17 12:34:03 -0500

    Initial Commit

---
+++ DoIt/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DoIt
+//
+//  Created by Charles Northup on 3/17/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
